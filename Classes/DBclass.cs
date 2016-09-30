16fb89415f9eca822383ae3df910313153eedff0
Umidjon Vakhidov <vumid85@mail.ru>
2016-09-30 17:20:37 +0400

    New project

---
+++ Classes/DBclass.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Data;
+using MySql.Data;
+using MySql.Data.MySqlClient;
+
+namespace foodsDesktop.Classes
+{
+    class DBclass
+    {
+        MySqlConnection connection;
+        MySqlDataAdapter adapter;
+
+        public static DataSet DS { get; set; }
+
+        public DBclass()
+        {
+            connection = new MySqlConnection("server=localhost;user id=foodsDB_user;password=D@faul(t);database=foods;persistsecurityinfo=True");
+            string[] tables = { "employee", "dishes", "halfstaff", "products" };
+            if (DS == null)
+                DS = new DataSet();
+            foreach(string tableName in tables)
+            Fill(tableName);
+        }
+        public DBclass(string name)
+        {
+            connection = new MySqlConnection("server=localhost;user id=foodsDB_user;password=D@faul(t);database=foods;persistsecurityinfo=True");
+            string[] tables = { "employee", "dishes", "halfstaff", "products" };
+            if (DS == null)
+                DS = new DataSet();
+            Fill(name);
+        }
+
+        public void Fill(string table_name)
+        {
+            adapter = new MySqlDataAdapter("select * from " + table_name, connection);
+            adapter.Fill(DS, table_name);
+
+            
+        }
+        
+        
+    }
+}
---
+++ LoginForm.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using foodsDesktop.Classes;
+using System.Windows.Forms;
+
+namespace foodsDesktop
+{
+    public partial class LoginForm : Form
+    {
+        
+        public LoginForm()
+        {
+            InitializeComponent();
+            int w = this.Size.Width;
+            int h = this.Size.Height;
+            label1.Location = new Point(w/2-175, label1.Location.Y);
+            label2.Location = new Point(w / 2 - 175, label2.Location.Y);
+            tbxLogin.Location = new Point(w / 2 - 175, tbxLogin.Location.Y);
+            tbxPass.Location = new Point(w / 2 - 175, tbxPass.Location.Y);
+            btnLogin.Location = new Point(w / 2 - 175, btnLogin.Location.Y);
+            DBclass DB = new Classes.DBclass("employee");
+        }
+
+        private void btnLogin_Click(object sender, EventArgs e)
+        {
+            DataTable table =  DBclass.DS.Tables["employee"];
+            string hash = CalculateMD5Hash(tbxPass.Text);
+            DataRow[] rows = table.Select("login='"+tbxLogin.Text+"' and password = '"+ tbxPass.Text+"'");
+            if (rows.Length != 0)
+            {
+                MessageBox.Show("OK");
+            }
+            else { MessageBox.Show("Ошибка входа...", "Логин или пароль не правильный", MessageBoxButtons.OK, MessageBoxIcon.Error); }
+            
+        }
+        public string CalculateMD5Hash(string input)
+        {
+
+            // step 1, calculate MD5 hash from input
+
+            MD5 md5 = System.Security.Cryptography.MD5.Create();
+            byte[] inputBytes = System.Text.Encoding.UTF8.GetBytes(input);
+            byte[] hash = md5.ComputeHash(inputBytes);
+
+            // step 2, convert byte array to hex string
+            StringBuilder sb = new StringBuilder();
+            for (int i = 0; i < hash.Length; i++)
+            {
+                sb.Append(hash[i].ToString("X2"));
+            }
+            return sb.ToString();
+        }
+
+    }
+}
